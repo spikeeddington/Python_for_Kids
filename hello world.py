46e8ce7c3606ac4459b78e1016b58f4883297e6a
Nicholas Eddington <nicholas.eddington@seattlecentral.edu>
2019-03-25 15:37:26 -0700

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,9 @@
+Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:44:43) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> help
+Type help() for interactive help, or help(object) for help about object.
+>>> print("hello world")
+hello world
+>>> 
